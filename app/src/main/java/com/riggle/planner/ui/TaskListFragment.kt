b926881fd2d58631e745740604368317b29f0a4e
Oleg <reffum@bk.ru>
2023-01-08 22:39:50 +0300

    Refactor. Move TaskListFragment.kt in ui

--- app/src/main/java/com/riggle/planner/ui/TaskListFragment.kt
+++ app/src/main/java/com/riggle/planner/ui/TaskListFragment.kt
@@ -1,10 +1,11 @@
-package com.riggle.planner
+package com.riggle.planner.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.riggle.planner.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
